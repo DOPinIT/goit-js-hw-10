95da504895fd299f659a7b0a36ce27d350cbd196
DOPinIT <illyakonor@gmail.com>
2023-02-10 13:52:31 +0200

    pCpush

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,11 @@
 export function fetchCountries(e) {
-  return  fetch(`https://restcountries.com/v3.1/name/${e}`).then((response) =>
-       response.json()
-    );
+  return  fetch(`https://restcountries.com/v3.1/name/${e}?fields=name.official,capital,population,flags.svg,languages`).then((response) =>
+       {if (!response.ok) {
+        throw new Error(response.status)
+       }
+    return response.json()
+}
+    ).then(data => console.log(data));
 };
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import lodash from "lodash.debounce"
 import {fetchCountries} from "./fetchCountries.js"
+
 
 
 const input = document.getElementById("search-box");
 console.log(input);
 const DEBOUNCE_DELAY = 300;
 
-fetchCountries("ukraine");
+input.addEventListener("input", lodash(inputFetchCountries, DEBOUNCE_DELAY))
+
+function inputFetchCountries({target}) {
+    if (target !== "") {
+        return fetchCountries(target.value)
+    }
+}
+
