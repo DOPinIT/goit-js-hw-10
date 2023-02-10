595a93ef54b6bc5ccd0da4119ab5d750014ec7fc
DOPinIT <111194712+DOPinIT@users.noreply.github.com>
2023-02-10 11:52:57 +0000

    Deploying to gh-pages from @ DOPinIT/goit-js-hw-10@95da504895fd299f659a7b0a36ce27d350cbd196 🚀

--- index.da29664e.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=document.getElementById("search-box");console.log(n);fetch("https://restcountries.com/v3.1/name/".concat("ukraine")).then((function(n){return n.json()}))}();
-//# sourceMappingURL=index.da29664e.js.map
