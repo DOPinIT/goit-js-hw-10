f0fffa4c7ebbede8ae9575f24d7bb9f4cb5177d5
DOPinIT <illyakonor@gmail.com>
2023-02-10 14:09:29 +0200

    pcoush2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(e) {
-  return  fetch(`https://restcountries.com/v3.1/name/${e}?fields=name.official,capital,population,flags.svg,languages`).then((response) =>
+  return  fetch(`https://restcountries.com/v3.1/name/${e}?fields=name.official,capital,population,flags,languages`).then((response) =>
        {if (!response.ok) {
         throw new Error(response.status)
        }
