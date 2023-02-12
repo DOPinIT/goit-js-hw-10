3fb32b186cd1e43f97ab7b7994a0d611964aa368
DOPinIT <illyakonor@gmail.com>
2023-02-12 17:48:21 +0200

    hw_10

--- src/index.js
+++ src/index.js
@@ -8,42 +8,76 @@ import {fetchCountries} from "./fetchCountries.js"
 const input = document.getElementById("search-box");
 const div = document.querySelector(".country-info");
 const ul = document.querySelector(".country-list");
-console.log(div);
-console.log(input);
+
 const DEBOUNCE_DELAY = 300;
 
 input.addEventListener("input", lodash(inputFetchCountries, DEBOUNCE_DELAY))
 
-function inputFetchCountries({target}) {
-  const input = target.value.trim();
+function inputFetchCountries(e) {
+  e.preventDefault()
+  const input = e.target.value.trim();
   console.log(input);
-  fetchCountries(input).then((ar) => {console.log(ar[0]);
-    if (ar.length > 10)
-        throw new Error(Notify.info("Too many matches found. Please enter a more specific name."));
-       return ar.reduce((markup, ar) => createMarkup(ar) + markup, 
-       "");
-}).then(updateCountrys).catch(error => { if (error !== 0) {
-    return Notify.failure("Oops, there is no country with that name")   
-}
-}).finally(countryCard)
-}
-
-function createMarkup({name, flags}) {
-return`
-<li class = "liCountry"><img src="${flags.svg}" width = "40" height="20"></img>
-<h2 class = "ulName">${name.official}</h2></li>`
-}
-
-function updateCountrys(markup) {
-  if (markup === "") {
-    input.innerHTML = "";
+  if (!input) {
+    resetData(ul)
+    resetData(div)
+    return;
   }
-  ul.innerHTML = markup;
+  fetchCountries(input).then((ar) => {console.log(ar[0]);
+    if (ar.length > 10) {
+      Error(Notify.info("Too many matches found. Please enter a more specific name."));
+    }
+    else if (ar.length >= 2 && ar.length <= 10){
+      resetData(ul)
+      createMarkup(ar)
+      resetData(div)
+      } else {
+        resetData(div)
+        countryCard(ar)
+        resetData(ul)
+      }
+}).catch(() => {
+  resetData(ul)
+resetData(div)
+   Notify.failure("Oops, there is no country with that name");
+  })
+} 
+
+function createMarkup(dataCountry) {
+  const markup = dataCountry
+    .map(({ name, flags }) => {
+      return `<li class="country-list__item">
+        <img class="country-list__img" src="${flags.svg}" alt="flag" />
+        <p class="country-list__text">${name.official}</p>
+      </li>`;
+    })
+    .join('');
+  return ul.insertAdjacentHTML('beforeend', markup);
 }
 
-function countryCard({name, capital, population, flags, languages}) {
-  return div.innerHTML = `<div><img src = "${flags.svg}"></img><h1>${name.official}</h1></div>
-  <div><h3>Capital: ${capital}</h3>
-  <h3>Population: ${population}</h3>
-  <h3>Languages: ${language}</h3></div>`
+function countryCard(dataCountry) {
+  const markup = dataCountry.map(({name, capital, population, flags, languages}) =>
+  {
+    return `
+<div class="country__flag">
+  <img class="country__img" src="${flags.svg} " alt="flag">
+  <p class="country__name">${name.official}</p>
+</div>
+<ul class="country__info">
+    <li class="country__item"> <b>Capital</b>:
+  <span class="country__span">${capital}</span>
+    </li>
+    <li class="country__item"> <b>Population</b>:
+  <span class="country__span">${population}</span>
+    </li>
+    <li class="country__item"> <b>Languages</b>:
+  <span class="country__span">${Object.values(languages).join(", ")}</span>
+    </li>
+</ul>`;
+  })
+  .join("");
+  return div.insertAdjacentHTML("beforeend", markup)
+}
+
+function resetData(el) {
+  el.innerHTML = "";
 }
