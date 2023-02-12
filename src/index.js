092f36c4ae98f9d8edc355f59474cc65dc2c8c11
DOPinIT <illyakonor@gmail.com>
2023-02-12 09:57:57 +0200

    renderCountry

--- src/index.js
+++ src/index.js
@@ -17,11 +17,33 @@ input.addEventListener("input", lodash(inputFetchCountries, DEBOUNCE_DELAY))
 function inputFetchCountries({target}) {
   const input = target.value.trim();
   console.log(input);
-  fetchCountries(input).then(({...name}) => {
-    if (name[0].length >= 10)
+  fetchCountries(input).then((ar) => {console.log(ar[0]);
+    if (ar.length > 10)
         throw new Error(Notify.info("Too many matches found. Please enter a more specific name."));
-    name.map({name, flags} => {return `<li><img src="${flags.svg}"></img><h3>${name.official}</h3></li>`})}).catch(Notify.failure("Oops, there is no country with that name"));
+       return ar.reduce((markup, ar) => createMarkup(ar) + markup, 
+       "");
+}).then(updateCountrys).catch(error => { if (error !== 0) {
+    return Notify.failure("Oops, there is no country with that name")   
+}
+}).finally(countryCard)
 }
 
+function createMarkup({name, flags}) {
+return`
+<li class = "liCountry"><img src="${flags.svg}" width = "40" height="20"></img>
+<h2 class = "ulName">${name.official}</h2></li>`
+}
 
+function updateCountrys(markup) {
+  if (markup === "") {
+    input.innerHTML = "";
+  }
+  ul.innerHTML = markup;
+}
 
+function countryCard({name, capital, population, flags, languages}) {
+  return div.innerHTML = `<div><img src = "${flags.svg}"></img><h1>${name.official}</h1></div>
+  <div><h3>Capital: ${capital}</h3>
+  <h3>Population: ${population}</h3>
+  <h3>Languages: ${language}</h3></div>`
+}
